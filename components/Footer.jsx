538e637c4c08c70c7b71613ac5dbddb5c395a068
Muhammad Ahmad <mohammad01ahmad@gmail.com>
2026-02-21 16:26:18 +0400

    slight changes in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -38,16 +38,29 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="flex justify-between border-t border-gray-800 pt-8 text-left text-gray-400">
-                    <p>Copyright © 2026 MedBlend. All rights reserved.</p>
-                    <div className='flex flex-row gap-4'>
-                        <div className="flex space-x-2">
-                            <a href="https://www.instagram.com/medblendapp/" className="hover:text-purple-400 transition-colors text-2xl"><IoLogoInstagram /></a>
-                            <p className='hover:text-purple-400 transition-colors'><a href="https://www.instagram.com/medblendapp/">Instagram</a></p>
+                {/* Make it mobile responsive */}
+                <div className="flex flex-col md:flex-row justify-between md:items-start gap-4 md:gap-0 border-t border-gray-800 pt-8 text-gray-400 text-sm sm:text-base">
+                    <p className="text-left">
+                        Copyright © 2026 MedBlend. All rights reserved.
+                    </p>
+
+                    <div className='flex flex-col sm:flex-row gap-4'>
+                        <div className="flex items-center space-x-2">
+                            <a href="https://www.instagram.com/medblendapp/" className="hover:text-purple-400 transition-colors text-2xl">
+                                <IoLogoInstagram />
+                            </a>
+                            <a href="https://www.instagram.com/medblendapp/" className='hover:text-purple-400 transition-colors'>
+                                Instagram
+                            </a>
                         </div>
-                        <div className="flex space-x-2">
-                            <a className="hover:text-purple-400 transition-colors text-2xl"><LuMail /></a>
-                            <p>medblendapp@gmail.com</p>
+
+                        <div className="flex items-center space-x-2">
+                            <a href="mailto:medblendapp@gmail.com" className="hover:text-purple-400 transition-colors text-2xl">
+                                <LuMail />
+                            </a>
+                            <a href="mailto:medblendapp@gmail.com" className='hover:text-purple-400 transition-colors'>
+                                medblendapp@gmail.com
+                            </a>
                         </div>
                     </div>
                 </div>
